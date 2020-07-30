c33d18af9b2548e284eb3bd911378667fb2c7b40
Derek Wang <wang.y.derek@gmail.com>
2020-07-30 19:05:58 -0400

    linear regressor with sklearn working bare minimum

--- AvocadoPricePrediction/SingleVarLinearRegressor.py
+++ AvocadoPricePrediction/SingleVarLinearRegressor.py
@@ -15,17 +15,20 @@ y = dataset.iloc[:, 0].values
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
 
+X_train = X_train.reshape(-1, 1)
+y_train = y_train.reshape(-1, 1)
 print(X_train)
+print(y_train)
 # Training the Simple Linear Regression model on the Training set
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
 
 # Predicting the Test set results
-y_pred = regressor.predict(X_test)
+y_pred = regressor.predict(X_test.reshape(-1, 1)[:200])
 
 # Visualising the Training set results
-plt.scatter(X_train, y_train, color = 'red')
+plt.scatter(X_train[:200], y_train[:200], color = 'red')
 plt.plot(X_train, regressor.predict(X_train), color = 'blue')
 plt.title('Avocado Price vs Total Volume (Training set)')
 plt.xlabel('Volume')
@@ -33,7 +36,7 @@ plt.ylabel('Price')
 plt.show()
 
 # Visualising the Test set results
-plt.scatter(X_test, y_test, color = 'red')
+plt.scatter(X_test[:200], y_test[:200], color = 'red')
 plt.plot(X_train, regressor.predict(X_train), color = 'blue')
 plt.title('Avocado Price vs Total Volume (Training set)')
 plt.xlabel('Volume')

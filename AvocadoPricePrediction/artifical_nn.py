5a48f8de5bc97c79f37620cfae088e4ae5de17a1
Derek Wang <wang.y.derek@gmail.com>
2020-08-28 14:24:35 -0400

    added base code for the ability to save neural nets feature

--- AvocadoPricePrediction/artifical_nn.py
+++ AvocadoPricePrediction/artifical_nn.py
@@ -19,6 +19,12 @@ class ArtificialBrain:
         self.ann.compile(optimizer='adam', loss='mean_squared_error')
         self.ann.fit(self.X_train, self.y_train, batch_size=40, epochs=150)
 
+    def save(self):
+        self.ann.save("ANN")
+
+    def load(self, path):
+        self.ann = tf.keras.models.load_model(path)
+
 
 if __name__ == "__main__":
     data = NeuralNetPreprocessor()
@@ -26,10 +32,12 @@ if __name__ == "__main__":
 
     # print(data.get_X_test())
     art_nn = ArtificialBrain(data.get_X_train(), data.get_y_train())
-    art_nn.create_net(6)
-    art_nn.train_brain()
+    # art_nn.create_net(6)
+    # art_nn.train_brain()
+    # art_nn.save()
 
     # test
+    art_nn.load("ANN")
     y_pred = art_nn.ann.predict(data.get_X_test())
     np.set_printoptions(precision=2)
     viewable = np.concatenate((y_pred.reshape(len(y_pred), 1),
